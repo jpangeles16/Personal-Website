20acccd7c694879c9815b27f0e9f09a984cef636
John Angeles <jpangeles16@gmail.com>
2019-08-12 01:35:22 -0700

    Test

--- navbar.js
+++ navbar.js
@@ -2,34 +2,36 @@
 @author W3Schools and John Angeles
 */
 
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+$(document).ready(function() {
+	// When the user scrolls the page, execute myFunction 
+	window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+	// Get the navbar
+	var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+	// Get the offset position of the navbar
+	var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function myFunction() {
+	  if (window.pageYOffset >= sticky) {
+	    navbar.classList.add("sticky")
+	  } else {
+	    navbar.classList.remove("sticky");
+	  }
+	}
 
-/* Next chunk allows us to scroll to different parts of our website. */
-var homeButton = document.getElementById("home");
+	/* Next chunk allows us to scroll to different parts of our website. */
+	var homeButton = document.getElementById("home");
 
-var experienceButton = document.getElementById("experience");
-var experienceLocation = $(".experience").position();
+	var experienceButton = document.getElementById("experience");
+	var experienceLocation = $(".experience").position();
 
-homeButton.onclick = function() {
-	window.scrollTo(0, 0, "smooth");
-}
+	homeButton.onclick = function() {
+		window.scrollTo(0, 0, "smooth");
+	}
 
-experienceButton.onclick = function() {
-	window.scrollTo(0, experienceLocation.top - 100, "smooth");
-}
+	experienceButton.onclick = function() {
+		window.scrollTo(0, experienceLocation.top - 100, "smooth");
+	}
+});
