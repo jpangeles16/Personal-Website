4b888b5d07a22472026bd863d97a51a94bf50d8a
John Angeles <jpangeles16@gmail.com>
2019-08-12 01:32:12 -0700

    Website update test

---
+++ navbar.js
@@ -0,0 +1,35 @@
+/* JavaScript borrowed from online to manage navbar.
+@author W3Schools and John Angeles
+*/
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+/* Next chunk allows us to scroll to different parts of our website. */
+var homeButton = document.getElementById("home");
+
+var experienceButton = document.getElementById("experience");
+var experienceLocation = $(".experience").position();
+
+homeButton.onclick = function() {
+	window.scrollTo(0, 0, "smooth");
+}
+
+experienceButton.onclick = function() {
+	window.scrollTo(0, experienceLocation.top - 100, "smooth");
+}
